b7ba725420a3bbe5975a0cdbb0bf1719d9442c01
ASADUJJAMANCODELIFE <67199735+ASADUJJAMANCODELIFE@users.noreply.github.com>
2021-03-01 18:45:58 +0600

    1st commit

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace Association
 
         static void Main(string[] args)
         {
-            Bank bank = new Bank("bank's for people", 5);// create bank object and define bank name , number of bank account
-            Address address = new Address("10", "22/A", " Dahaka ", " bangladesh");//assign address value
+            Bank bank = new Bank("Bank's for people", 5);// create bank object and define bank name , number of bank account
+            Address address = new Address("10", "1230/A", " Dhaka ", " Bangladesh");//assign address value
             Account account = new Account("prince", 5000, address); // create list account object
 
             bank.AddAccount(account1); // add 1st created account
