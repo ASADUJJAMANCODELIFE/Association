3c58d797b7d4ab6da150605e5f24f5cd8d502207
ASADUJJAMANCODELIFE <67199735+ASADUJJAMANCODELIFE@users.noreply.github.com>
2021-03-02 21:58:32 +0600

    commit

--- Bank.cs
+++ Bank.cs
@@ -17,22 +17,67 @@ namespace Association
             this.myBank = new Account[size];
 
         }
-        public void Addaccount()
+        public void AddAccount(Account account)
         {
+            for (int i = 0; i < myBank.Length; i++)
+            {
 
+                if (myBank[i] == null)
+                {
+                    myBank[i] = account;
+                    break;
+                }
+            }
         }
 
-        public void DeleteAccount()
+        public void DeleteAccount(int accountNo)
         {
+            int count = 0;
+            for (int i = 0; i < myBank.Length; i++)
+            {
 
+                if (myBank[i].AccountNumber == accountNo)
+                {
+                    for (int j = 0; j < (myBank.Length - 1); j++)
+                    {
+                        myBank[i] = myBank[j = 1];
+                        break;
+                    }
+                    count++;
+                    break;
+                }
+
+            }
         }
             public void Transaction()
         {
 
         }
-            public void PrintAccountDetails()
-        {
 
+        public void PrintAccountDetails(int accountNo)
+        {
+            for (int i = 0; i < myBank.Length; i++)
+            {
+
+                if(myBank[i].AccountNumber == accountNo)
+                {
+                    myBank[i].ShowAccountInformation();
+                    break;
+                }
+            }
+        }
+
+        public void PrintAllAccount()
+        {
+            for (int i = 0; i < myBank.Length; i++)
+            {
+
+                if (myBank[i] == null)
+                {
+                    myBank[i].ShowAccountInformation();
+                    
+                }
+            }
         }
 
     }
